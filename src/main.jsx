3c804566719a8186252594d653a64497b95b06a8
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-03-25 22:32:58 +0900

    星評価コンポーネントに新しいプロパティを追加し、デフォルト評価やカスタムメッセージのサポートを実装

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { useState } from "react";
 // import './index.css'
 // import App from './App.jsx'
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>この映画は星{movieRating}個です</p>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating maxRating={5} messages={["a", "b", "c", "d", "e"]} />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </StrictMode>
 );
